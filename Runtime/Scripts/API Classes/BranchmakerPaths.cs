f4785947d8d6662b6d41528bf2e3e6671e27a821
Nils Munch <gecko@wolflord.dev>
2023-01-19 15:18:57 +0100

    Fix path definitions and double-slash bug
    Signed-off-by: Nils Munch <gecko@wolflord.dev>

--- Runtime/Scripts/API Classes/BranchmakerPaths.cs
+++ Runtime/Scripts/API Classes/BranchmakerPaths.cs
@@ -7,14 +7,13 @@ namespace BranchMaker.Api
     public class BranchmakerPaths
     {
         const string serverRoot = "https://branchmaker.com/";
-        static public string jsonurl = serverRoot + "client/storynodes/";
-        public static string jsonorderurl = serverRoot + "suggestions/forceblock/";
+        public static string jsonorderurl = serverRoot + "api/{0}/feed/story.json";
         public static string jsonupdateurl = serverRoot + "suggestions/updateblock/";
         public static string suggesturl = serverRoot + "suggestions/playersuggest/";
         
         public static string StoryNodes(bool staticPath, string storyId)
         {
-            return serverRoot+"/api/"+storyId+"/feed/story.json";
+            return string.Format(jsonorderurl,storyId);
             return serverRoot + (staticPath ? "static/" : "") +"client/storynodes/";
         }
     }
