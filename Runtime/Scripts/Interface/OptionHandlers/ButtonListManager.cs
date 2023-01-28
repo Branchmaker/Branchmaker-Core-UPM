b510c8b527d43ad62291dbc49a609f5d4b7b89c4
Nils Munch <gecko@wolflord.dev>
2023-01-28 22:34:15 +0100

    Button list manager

---
+++ Runtime/Scripts/Interface/OptionHandlers/ButtonListManager.cs
@@ -0,0 +1,77 @@
+using System.Collections.Generic;
+using System.Linq;
+using BranchMaker;
+using BranchMaker.Story;
+using BranchMaker.UI;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonListManager : MonoBehaviour, IOptionHandler
+{
+    private static ButtonListManager manager;
+    private List<DialogueButton> _actionButtons;
+    public Sprite defaultActionIcon;
+    public void Awake()
+    {
+        manager = this;
+        _actionButtons = FindObjectsOfType<DialogueButton>(true).ToList();
+    }
+
+    public bool CanHandleBlock(BranchNodeBlock block)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void ProcessNode(BranchNode node)
+    {
+        var buttonIndex = 0;
+        Cleanup();
+
+        if (StoryManager.HasSpeakingQueue()) return;
+        if (StoryManager.IsCurrentlyWriting()) return;
+
+        if (StoryManager.manager.clickToContinue != null) StoryManager.manager.clickToContinue.SetActive(false);
+
+        foreach (var block in node.ActionBlocks())
+        {
+            if (StorySceneManager.SceneHasNodeButton(block.target_node)) continue;
+            if (!StoryEventManager.ValidBlockCheck(block)) continue;
+            if (block.clean_action.StartsWith("#") && StoryManager.manager.HideScriptActions) continue;
+
+            var buttonLabel = block.dialogue.CapitalizeFirst();
+            if (!string.IsNullOrEmpty(block.meta_scripts))
+            {
+                if (block.meta_scripts.Contains("needword:"))
+                    buttonLabel = "<color=#00FFFF>" + buttonLabel + "</color>";
+            }
+
+            manager._actionButtons[buttonIndex].gameObject.SetActive(true);
+            manager._actionButtons[buttonIndex].BroadcastMessage("SetLabel", buttonLabel);
+            
+            if (manager._actionButtons[buttonIndex].gameObject.transform.Find("Icon") != null)
+            {
+                var icon = StoryEventManager.BlockIcon(block);
+                manager._actionButtons[buttonIndex].gameObject.transform.Find("Icon").GetComponent<Image>().sprite =
+                    (icon == null ? manager.defaultActionIcon : icon);
+            }
+
+            manager._actionButtons[buttonIndex].GetComponent<Button>().onClick.RemoveAllListeners();
+            manager._actionButtons[buttonIndex].GetComponent<Button>().onClick.AddListener(
+                () => { StoryManager.LoadNodeKey(block.target_node); });
+            buttonIndex++;
+        }
+
+        foreach (var dialogueOption in StoryManager.manager._customDialogueOptions)
+        {
+            dialogueOption.ProcessDialogueOptions(node);
+        }
+    }
+
+    public void Cleanup()
+    {
+        foreach (var but in manager._actionButtons)
+        {
+            but.gameObject.SetActive(false);
+        }
+    }
+}
