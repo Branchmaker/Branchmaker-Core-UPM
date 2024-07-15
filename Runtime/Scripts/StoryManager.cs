b9d1af51089cc741384a24be8afce2f64141b845
Nils Munch <gecko@wolflord.dev>
2024-07-15 15:36:12 +0200

    Update StoryManager.cs

--- Runtime/Scripts/StoryManager.cs
+++ Runtime/Scripts/StoryManager.cs
@@ -48,7 +48,7 @@ namespace BranchMaker
         public bool HideScriptActions = true;
 
         [Header("Events")]
-        [NonSerialized] public static UnityEvent OnStoryReady;
+        [NonSerialized] public static UnityEvent OnStoryReady = new();
         [NonSerialized] public readonly UnityEvent<BranchNode> OnNodeChange = new();
         [NonSerialized] public readonly UnityEvent<BranchNode> OnNodeComplete = new();
         [NonSerialized] public readonly UnityEvent<BranchNodeBlock> OnBlockChange = new();
