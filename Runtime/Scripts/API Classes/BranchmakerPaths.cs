0e8921719028f2e6ed7472394930963c1cff5019
Nils Munch <gecko@wolflord.dev>
2025-06-04 17:21:32 +0200

    Branchmaker Paths

--- Runtime/Scripts/API Classes/BranchmakerPaths.cs
+++ Runtime/Scripts/API Classes/BranchmakerPaths.cs
@@ -1,20 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 namespace BranchMaker.Api
 {
-    public class BranchmakerPaths
+    public static class BranchmakerPaths
     {
-        const string serverRoot = "https://branchmaker.com/";
+        const string serverRoot = "https://api.ranchmaker.com/";
         public static string jsonorderurl = serverRoot + "api/{0}/feed/story.json";
         public static string jsonupdateurl = serverRoot + "suggestions/updateblock/";
         public static string suggesturl = serverRoot + "suggestions/playersuggest/";
+
+        public enum PluginApi
+        {
+            Suggestions,
+        }
         
         public static string StoryNodes(bool staticPath, string storyId)
         {
             return serverRoot+"api/"+storyId+"/feed/story.json";
-            //return serverRoot + (staticPath ? "static/" : "") +"client/storynodes/";
+        }
+        
+        
+        public static string PluginApiRoute(PluginApi type)
+        {
+            return type switch
+            {
+                PluginApi.Suggestions => serverRoot + "api/steam/suggestion",
+                _ => null
+            };
         }
     }
 }
