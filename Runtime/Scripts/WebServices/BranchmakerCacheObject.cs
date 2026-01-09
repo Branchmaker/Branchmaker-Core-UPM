375a9af29d8ed9304451cbc9e378383fa3d58d1b
Nils Munch <gecko@wolflord.dev>
2026-01-09 16:49:25 +0100

    Nodecount and update timestamps on cache objects

--- Runtime/Scripts/WebServices/BranchmakerCacheObject.cs
+++ Runtime/Scripts/WebServices/BranchmakerCacheObject.cs
@@ -1,10 +1,15 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace BranchMaker.WebServices
 {
     [CreateAssetMenu]
     public class BranchmakerCacheObject : ScriptableObject
     {
+        public DateTime LastUpdateDate;
+        
+        public int NodeCount;
+        
         public string cacheUrl;
         [TextArea(2,30)]
         public string cacheData;
