755127e3fd82f6aab927e1367292fe3ac9461107
Nils Munch <gecko@wolflord.dev>
2026-06-24 08:46:04 +0200

    Parsing the actor objects

--- Runtime/Scripts/Actors/ActorObject.cs
+++ Runtime/Scripts/Actors/ActorObject.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-
 namespace BranchMaker.Actors
 {
     [CreateAssetMenu]
@@ -24,9 +23,7 @@ namespace BranchMaker.Actors
 
             if (string.IsNullOrEmpty(CurrentEmotion)) return expressions.First().characterImage;
             foreach (var expression in expressions.Where(expression => expression.expression == CurrentEmotion))
-            {
                 return expression.characterImage;
-            }
 
             return mainSprite;
         }
@@ -35,20 +32,52 @@ namespace BranchMaker.Actors
         {
             CurrentEmotion = string.Empty;
         }
-    }
 
-    [Serializable]
-    public class CharacterExpression
-    {
-        public string expression;
-        public Sprite characterImage;
-    }
+        public Dictionary<string, Sprite> ParseExpressionString(string input)
+        {
+            var requested = new Dictionary<string, string>();
 
-    [Serializable]
-    public class CharacterExpressionSet
-    {
-        public string prefix;
-        public List<CharacterExpression> expressions;
-        public bool startsEmpty;
+            if (!string.IsNullOrWhiteSpace(input))
+                foreach (var part in input.Split('+'))
+                {
+                    var pieces = part.Split(':');
+
+                    if (pieces.Length == 2) requested[pieces[0].Trim()] = pieces[1].Trim();
+                }
+
+            var result = new Dictionary<string, Sprite>();
+
+            foreach (var layer in expressionLayers)
+            {
+                Sprite sprite = null;
+
+                if (requested.TryGetValue(layer.prefix, out var requestedKey))
+                {
+                    var match = layer.expressions?
+                        .FirstOrDefault(x => x.expression == requestedKey);
+
+                    if (match != null) sprite = match.characterImage;
+                }
+
+                result[layer.prefix] = sprite;
+            }
+
+            return result;
+        }
+
+        [Serializable]
+        public class CharacterExpression
+        {
+            public string expression;
+            public Sprite characterImage;
+        }
+
+        [Serializable]
+        public class CharacterExpressionSet
+        {
+            public string prefix;
+            public List<CharacterExpression> expressions;
+            public bool startsEmpty;
+        }
     }
 }
